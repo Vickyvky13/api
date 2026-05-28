43d8a98302b1cf959ba98be3084af96cfe94daf7
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2026-05-28 21:33:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # config.py  ─  fill in your actual values before running
 
 # ── API Server ──────────────────────────────────────
-API_URL = "http://localhost:8000"       # URL where server.py runs
+API_URL = "http://72.62.252.37:8000"       # URL where server.py runs
 API_KEY = "SOLO"                        # Secret key (same in server + client)
 
 # ── Telegram ────────────────────────────────────────
-BOT_TOKEN   = "123456:ABCdef..."        # Your bot token from @BotFather
-TELEGRAM_CHANNEL = "@apisolotreee"      # Channel username or numeric id (-100...)
+BOT_TOKEN   = "6499749567:AAGPN8lK7Gy3KaM7Am7JLKSQrzSu8Uc0E7g"        # Your bot token from @BotFather
+TELEGRAM_CHANNEL = "@apisolotreee13"      # Channel username or numeric id (-100...)
 # Bot must be admin in the channel with "Post Messages" permission
 
 # ── MongoDB ─────────────────────────────────────────
-MONGO_URI     = "mongodb://localhost:27017"   # or your Atlas URI
+MONGO_URI     = "mongodb+srv://sengalpsycho1303:15406578@cluster0.janupoy.mongodb.net/?appName=Cluster0"   # or your Atlas URI
 MONGO_DB_NAME = "solo_music"
